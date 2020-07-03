bc38dd274f7fec1bbca875846d338fd32dc686b7
aswinchand <66733099+aswinchand@users.noreply.github.com>
2020-07-03 14:23:13 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,54 +1,48 @@
-from functools import lru_cache
-
-@lru_cache(maxsize = 1000)
-
-
 class Node:
-    def __init__(self, data):
-        self.data=data
-        self.next=None
+    def __init__(self, data, next = None):
+        self.val = data
+        self.next = next
 
-class Linked_list:
-    def __init__(self):
-        self.head=None
+def insert(data):
+    head = Node(data[0])
+    for n in data[1:]:
+        ptr = head
+        while ptr.next:
+            ptr = ptr.next
+        ptr.next = Node(n)
 
-   
+    return head
 
-    def append(self,data):
-        new_node= Node(data)
-        if self.head is None:
-            self.head=new_node
-            return
-        current_node=self.head
-        while True:
-            if current_node.next is None:
-                current_node.next=new_node
-                break
-            current_node=current_node.next
-       
-    def display(self):
-       cur_node=self.head
-       while cur_node:
-           print(cur_node.data)
-           cur_node=cur_node.next
-
-def fibseries(n):
-    if n==0:
-        return 0
-    elif n==1:
-        return 1
-    elif n==2:
-        return 1
-    elif n>2:
-        return fibseries(n-1) + fibseries(n-2)
+def display(head):
+    ptr = head
+    while ptr:
+        print(ptr.val, end = ", ")
+        ptr = ptr.next
 
 
-my_list = Linked_list()
-for n in range(0,100):
-    #print(fibseries(n))
-    my_list.append(fibseries(n))
-my_list.display()
+class LinkedList(object):
+    def reverse(self, head):
+        return  self.solve(head,None)
 
-
-
-
+    def solve(self, head, back):
+        if not head:
+            return head
+        temp= head.next
+        head.next = back
+        back = head
+        if not temp:
+            return head
+        head = temp
+        return self.solve(head,back)
+        
+fibnumbers = [0, 1]
+for i in range(2,100):
+    fibnumbers.append(fibnumbers[i-1]+fibnumbers[i-2])
+#n=len(fibnumbers)
+list1 = insert(fibnumbers)
+mylist = LinkedList()
+#print("The original list :\n")
+#display(list1)
+list2 = mylist.reverse(list1)
+print("\nThe new list :\n")
+display(list2)
